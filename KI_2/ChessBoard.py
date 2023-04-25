b869070a41ea841a7f53a484b5ef7f6cbaf26d5e
Baran <Baran_ozbey02@outlook.de>
2023-04-25 21:23:31 +0200

    .

---
+++ KI_2/ChessBoard.py
@@ -0,0 +1,114 @@
+from threading import Thread
+import pygame
+import time
+
+#set color with rgb
+white,black,red,gray = (255,255,255),(0,0,0),(255,0,0),(100,100,100)
+boardLength = 8
+screenSize = (800,800)
+#Size of squares
+size = 80
+
+class Field:
+    def __init__(self,row,col):
+        self.row = row
+        self.col = col
+        self.color = white
+        self.queen = False
+    
+    def setBlack(self):
+        self.color = black
+    def setWhite(self):
+        self.color = white
+    def setQueen(self):
+        self.queen = True
+    def isWhite(self):
+        return self.color == white
+    def isBlack(self):
+        return self.color == black
+    def isQueen(self):
+        return self.queen
+    
+
+
+class Board:
+    def __init__(self, queenPositions,queenQuantitys):
+        self.x = queenPositions
+        self.y = queenQuantitys
+        self.board = [[0]*boardLength for i in range(boardLength)]
+        self.initialzeFields()
+
+    def initialzeFields(self):
+        #board length, must be even
+        cnt = 0
+        for row in range(boardLength):
+            for col in range(boardLength):
+                self.board[row][col] = Field(row,col)
+                if cnt % 2 == 0:
+                    self.board[row][col].setBlack()
+                cnt +=1
+            #since theres an even number of squares go back one value
+            cnt-=1
+
+
+    def geneticAlgorithm(self):
+        
+
+    def main(self):
+        pygame.init()
+
+        #set display
+        self.gameDisplay = pygame.display.set_mode((800,800))
+
+        #caption
+        pygame.display.set_caption("ChessBoard")
+
+        #beginning of logic
+        gameExit = False
+
+        #Size of squares
+        size = 80
+        
+        #Image of the Queen
+        self.queenImg = pygame.image.load('QueenImage.png')
+
+        boardLength = 8
+        self.gameDisplay.fill(white)
+
+        print("HALOOO")
+        while not gameExit:      
+            self.drawBoard()
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    gameExit = True
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    pass
+
+        #quit from pygame & python
+        pygame.quit()
+        quit()
+
+    def drawBoard(self):
+        for row in range(boardLength):
+            for col in range(boardLength):
+            #check if current loop value is even
+                if(self.board[row][col].isWhite()):
+                    pygame.draw.rect(self.gameDisplay, white, [size*(row + 1),size*(col + 1),size,size])
+                else:
+                    pygame.draw.rect(self.gameDisplay, gray, [size*(row + 1),size*(col + 1),size,size])
+                if(self.board[row][col].isQueen()):
+                    self.gameDisplay.blit(self.queenImg,(size*(row + 1),size*(col + 1)))
+
+        #border
+        pygame.draw.rect(self.gameDisplay,black,[size,size,boardLength*size,boardLength*size],2)
+        pygame.draw.rect(self.gameDisplay,black,[size-10,size-10,boardLength*size + 20,boardLength*size + 20],2)
+        pygame.display.update()
+
+    def drawQueen(gameDisplay,queenImg):
+        gameDisplay.blit(queenImg, (500,500))
+
+b = Board(0,0)
+b.main()
+
